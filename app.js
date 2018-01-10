8a1c3d019e82328ab33cb7464bc0a78e7edeccae
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2018-01-09 21:03:13 -0800

    dropdown menu css and functionality

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function showMenu() {
+    $('.dropdown-nav').click(function () {
+        $('.dropdown').toggle();
+    })
+};
+
+function hideMenu() {
+    $('li').on('click', function () {
+        $('.dropdown').hide();
+    })
+};
+
+$(function () {
+    showMenu();
+    hideMenu();
+})
