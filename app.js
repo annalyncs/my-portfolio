bcfdaa6c1624c9e4d8f372ae37a586811db5eab3
Annalyn Sarmiento <annalyn.c.s@gmail.com>
2018-01-10 20:13:20 -0800

    add scroll JS

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 'use strict'
 
+let landing = $('#landing').offset();
+let about = $('#about').offset();
+let projects = $('#projects').offset();
+let skills = $('#skills').offset();
+let contact = $('#contact').offset();
+
+
+$(window).scroll(function () {
+    let screenPosition = $(document).scrollTop();
+
+    if (screenPosition < landing.top) {
+        $('a').removeClass('selected');
+    } else if (screenPosition < about.top) {
+        $('a').removeClass('selected');
+        $('.about-nav').addClass('selected');
+    } else if (screenPosition < projects.top) {
+        $('a').removeClass('selected');
+        $('.projects-nav').addClass('selected');
+    } else if (screenPosition < skills.top) {
+        $('a').removeClass('selected');
+        $('.skills-nav').addClass('selected');
+    } else if (screenPosition < contact.top) {
+        $('a').removeClass('selected');
+        $('.contact-nav').addClass('selected');
+    }
+})
+
+
 function showMenu() {
     $('span').click(function () {
         $('.dropdown').toggleClass('active');
@@ -8,7 +36,8 @@ function showMenu() {
 
 function selectedSection() {
     $('nav').on('click', 'a', function () {
-        $(this).toggleClass('selected');
+        $('a').removeClass('selected');
+        $(this).addClass('selected');
     })
 }
 
